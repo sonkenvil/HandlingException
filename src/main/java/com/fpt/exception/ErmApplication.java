edacb3e4ffa5db9259e3ff703991d5fd5de733db
Nguyen Hung Son (FHN.ECR) <SonNH38@fsoft.com.vn>
2019-01-22 13:35:27 +0700

    create project

---
+++ src/main/java/com/fpt/exception/ErmApplication.java
@@ -0,0 +1,14 @@
+package com.fpt.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErmApplication.class, args);
+	}
+
+}
+
